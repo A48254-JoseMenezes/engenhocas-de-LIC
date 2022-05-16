25c678b090ce06b74a374c365a3f1a975c36f4eb
A48254-JoseMenezes <jmgilmenezes17@gmail.com>
2022-05-16 09:59:42 +0100

    write fixed (*-*)

--- src/main/kotlin/TUI.kt
+++ src/main/kotlin/TUI.kt
@@ -2,25 +2,15 @@ object TUI {
 
     const val SIZE_LINE_LCD = 15
 
-    enum class Location(val offset:Int){ LEFT(0), CENTER(7), RIGHT(14) }
+    enum class Location(val offset:Int){ LEFT(0), CENTER(7), RIGHT(10) }
 
 
     fun write(text: String, line: Int, location: Location){
         LCD.clear()
-        if(text.length > SIZE_LINE_LCD ){
-            val lowSubstring = text.substring(startIndex = 0,endIndex = SIZE_LINE_LCD)
-            LCD.cursor(0,0)
-            LCD.write(lowSubstring)
+        LCD.cursor(line,location.offset)
+        LCD.write(text)
 
-            val highSubstring = text.substring(SIZE_LINE_LCD)
-            LCD.cursor(1,0)
-            LCD.write(highSubstring)
-
-        }
-        else{
-            LCD.write(text)}
     }
-}
 
 
 
