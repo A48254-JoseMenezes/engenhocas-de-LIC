7009eb0452ee57d7b5cb2324afe10ea37d7f39b4
joaobonachoA49437 <A49437@alunos.isel.pt>
2022-05-16 08:12:35 +0100

    new version

--- src/main/kotlin/LCD.kt
+++ src/main/kotlin/LCD.kt
@@ -1,30 +1,43 @@
-object LCD { // Escreve no LCD usando a interface a 8 bits.
-    // Dimensão do display.
+// Escreve no LCD usando a interface a 8 bits
+object LCD {
+    // Dimensão do display
     private const val LINES = 2
     private const val COLS = 16
 
     // Escreve um byte de comando/dados no LCD em série
     private fun writeByteSerial(rs: Boolean, data: Int) =
         SerialEmitter.send(SerialEmitter.Destination.LCD, (if (rs) 1 else 0).shl(8) + data)
+
     // Escreve um byte de comando/dados no LCD
     private fun writeByte(rs: Boolean, data: Int) =
         writeByteSerial(rs, data)
+
     // Escreve um comando no LCD
     private fun writeCMD(data: Int) =
         writeByte(false, data)
+
     // Escreve um dado no LCD
     private fun writeDATA(data: Int) =
         writeByte(true, data)
 
     // Envia a sequência de iniciação para comunicação a 8 bits.
     fun init() {
+        SerialEmitter.init()
+
         Thread.sleep(15, 5000)
+
         writeCMD(1.shl(5) + 1.shl(4))
+
         Thread.sleep(4, 5000)
+
         writeCMD(1.shl(5) + 1.shl(4))
+
         Thread.sleep(0, 500000)
+
         writeCMD(1.shl(5) + 1.shl(4))
+
         writeCMD(1.shl(5) + 1.shl(4) + 1.shl(3))
+        writeCMD(1.shl(3))
         writeCMD(1)
         writeCMD(1.shl(2) + 1.shl(1))
     }
@@ -55,5 +68,5 @@ object LCD { // Escreve no LCD usando a interface a 8 bits.
 
 fun main(){
     LCD.init()
-    LCD.write("Uma palavra.")
+    LCD.write("U")
 }
