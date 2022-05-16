878af16a000506a6dbb6d3adc452e67487db29e9
A48254-JoseMenezes <jmgilmenezes17@gmail.com>
2022-05-16 09:59:56 +0100

    write fixed (*-*)

--- src/main/kotlin/TUI.kt
+++ src/main/kotlin/TUI.kt
@@ -2,17 +2,17 @@ object TUI {
 
     const val SIZE_LINE_LCD = 15
 
-    enum class Location(val offset:Int){ LEFT(0), CENTER(7), RIGHT(10) }
+    enum class Location(val offset: Int) { LEFT(0), CENTER(7), RIGHT(10) }
 
 
-    fun write(text: String, line: Int, location: Location){
+    fun write(text: String, line: Int, location: Location) {
         LCD.clear()
-        LCD.cursor(line,location.offset)
+        LCD.cursor(line, location.offset)
         LCD.write(text)
 
     }
 
-
+}
 
 fun main(){
     TUI.write("hellloo", 1, TUI.Location.LEFT)
